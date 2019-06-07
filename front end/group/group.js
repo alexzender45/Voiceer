53bd62ff331de54d8febc2b63129130c6678ade8
alexzender45 <alexzendersamuel33@gmail.com>
2019-06-07 08:00:40 -0700

    added group page

---
+++ front end/group/group.js
@@ -0,0 +1,32 @@
+const select = document.getElementById('select');
+const grid = document.getElementById('grid');
+const show = document.getElementById('display');
+const icon1 = document.getElementById('.icon1');
+const icon2 = document.getElementById('.icon2');
+select.style.display = "none";
+
+//toggle for 1
+
+function toggle1() {
+    if(grid.style.display === "block"){
+        
+        grid.style.display = "block";
+        select.style.display = "none";
+    }else{
+        grid.style.display = "block";
+        select.style.display = "none";
+    }
+}
+
+//toggle for 2
+
+function toggle2() {
+    if(select.style.display === "none"){
+        grid.style.display = "none";
+        select.style.display = "block";
+    }  
+}
+
+
+// for icon1
+
