65e687de387f5605daf64c961523fac08905e6e3
alexzender45 <alexzendersamuel33@gmail.com>
2019-06-08 11:33:52 -0700

    responsiveness

--- front end/group/group.js
+++ front end/group/group.js
@@ -3,7 +3,32 @@ const grid = document.getElementById('grid');
 const show = document.getElementById('display');
 const icon1 = document.getElementById('.icon1');
 const icon2 = document.getElementById('.icon2');
+const fade1 = document.getElementsByClassName('fade1');
 select.style.display = "none";
+
+
+let check =[]
+for(let i = 0; i < fade1.length; i++){
+    check.push(fade1[i]);
+}
+
+console.log(check);
+//for search
+
+function display(){
+    let input = document.getElementById('form').value;
+    let x = document.getElementsByClassName('voiceer');
+    for(let i = 0; i < x.length; i++){
+        if(!x[i].innerHTML.toLowerCase().includes(input.toLowerCase())){
+            x[i].style.display = "none";
+            check[i].style.display = "none";
+        }else{
+            x[i].style.display = "block"; 
+            check[i].style.display = "list-item";
+            
+        }
+    }
+}
 
 //toggle for 1
 
